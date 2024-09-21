9a1b3fc8aa5151eff87cd33549ac0c2e9ec0153b
MayowaObisesan <mayowaobi74@gmail.com>
2024-09-21 04:46:31 -0700

    Linkedin clone - First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import 'boxicons'
+import ProfileCard from './components/ProfileCard';
+import CreatePost from './components/CreatePost';
+import Post from './components/Post';
+
+const postData = [
+  {
+    name: "Mayowa Obisesan",
+    bio: "Software Engineer | Frontend Developer | Web3 Mentor at web3bridge",
+    text: "This is the dummy text that should show for the first post component"
+  },
+  {
+    name: "User one",
+    bio: "This is user one bio",
+  },
+  {
+    name: "User two",
+    bio: "This is user two bio",
+  },
+  {
+    name: "User three",
+    bio: "This is user three bio",
+  },
+  {
+    name: "User four",
+    bio: "This is user four bio",
+  },
+  {
+    name: "User five",
+    bio: "This is user five bio",
+  },
+  {
+    name: "User six",
+    bio: "This is user six bio",
+  },
+  {
+    name: "User seven",
+    bio: "This is user seven bio",
+  },
+  {
+    name: "Mayowa Obisesan",
+    bio: "Software Engineer | Frontend Developer | Web3 Mentor at web3bridge",
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <div className='content-container flex flex-row gap-x-8 mt-8 mx-auto max-w-6xl'>
+
+        {/* The Left side of the body */}
+        <div>
+          <ProfileCard />
+        </div>
+
+        {/* The Center contents of the body */}
+        <div className='w-[60%] max-w-[60%]'>
+          <CreatePost />
+
+          <div className='space-y-2 py-2'>
+            {
+              postData.map((eachPost) => (
+                <Post {...eachPost} />
+              ))
+            }
+          </div>
+        </div>
+
+        {/* The Right side of the body */}
+        <div>
+          <ProfileCard />
+        </div>
+      </div>
     </div>
   );
 }
